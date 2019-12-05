40c6b32bb87c05cf6c6c086dab4b94e0830d4118
umisora <bit72s_akm@hotmail.com>
2019-12-06 07:11:07 +0900

    Add REST API Sample
    refs :
    https://medium.com/nyle-engineering-blog/flask%E3%81%A7rest-api%E3%82%92%E4%BD%9C%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B-fad8ae1fde5c

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
-import flask 
+import flask
 from main import app
+
 
 @app.route('/')
 def show_entries():
     return 'Hello, World!'
+
+
+@app.route('/hello')
+def hello_world():
+    return flask.jsonify({'message': 'Hello, world'})
